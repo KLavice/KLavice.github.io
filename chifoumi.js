f5fc611b6d3bd4466ac72d6ca3867226422f9c86
KLavice <lavice.kevin@gmail.com>
2024-04-05 12:27:12 +0200

    essai 1

---
+++ chifoumi.js
@@ -0,0 +1,53 @@
+var player;
+var random;
+var computer;
+
+player = window.prompt('Choisissez pierre feuille ou ciseaux');
+player = player.toLowerCase();
+random = Math.random();
+
+if(random < 1 / 3)
+{
+    computer = 'pierre';
+}
+
+else if(random < 2 / 3) 
+{
+    computer = 'feuille';
+}
+
+else
+{
+    computer = 'ciseaux';
+}
+
+
+document.write("<p>Choix de l'ordinateur : <strong>" + computer + "</strong></p>");
+if(player==computer)
+{
+	document.write("égalité");
+	
+}
+else
+{
+	switch(computer)
+	{case "pierre" : 
+		if (player == "feuille")  
+		{document.write("gagné")}
+		else
+		{document.write("perdu")}
+		break;
+	case "feuille" :
+        if (player == "pierre")  
+		{document.write("gagné")}
+		else
+		{document.write("perdu")}
+		break;
+    case "ciseaux" :
+            if (player == "pierre")  
+            {document.write("gagné")}
+            else
+            {document.write("perdu")}
+            break;
+		}
+}
