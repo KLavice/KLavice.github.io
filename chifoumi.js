8371390da05b05abc2cec292abf22a64da3512d7
KLavice <lavice.kevin@gmail.com>
2024-05-18 19:08:23 +0200

    changements chifoumi

--- chifoumi.js
+++ chifoumi.js
@@ -2,7 +2,7 @@ var player;
 var random;
 var computer;
 
-player = window.prompt('Choisissez pierre, feuille ou ciseaux');
+player = window.prompt("Ecrivez pierre, feuille ou ciseaux");
 player = player.toLowerCase();
 random = Math.random();
 
@@ -21,33 +21,60 @@ else
     computer = 'ciseaux';
 }
 
+document.write("<h2>Choix de l'ordinateur : <strong>" + computer + "</strong></h2>");
 
-document.write("<p>Choix de l'ordinateur : <strong>" + computer + "</strong></p>");
 if(player==computer)
 {
-	document.write("égalité");
-	
+	document.write("<h1>Egalité</h1>");
+	var image = document.createElement("img");
+	image.src = "images/balance.gif";
+	document.body.appendChild(image);
 }
+
 else
 {
 	switch(computer)
 	{case "pierre" : 
 		if (player == "feuille")  
-		{document.write("gagné")}
+			{document.write("<h1>Gagné !</h1>");
+			var image2 = document.createElement("img");
+			image2.src = "images/trophy.gif";
+			document.body.appendChild(image2);
+			}
 		else
-		{document.write("perdu")}
+			{document.write("<h1>Perdu !</h1>");
+			var image3 = document.createElement("img");
+			image3.src = "images/triste.gif";
+			document.body.appendChild(image3);
+			}
 		break;
 	case "feuille" :
         if (player == "ciseaux")  
-		{document.write("gagné")}
+			{document.write("<h1>Gagné !</h1>");
+			 image2 = document.createElement("img");
+			 image2.src = "images/trophy.gif";
+			 document.body.appendChild(image2);
+			}
 		else
-		{document.write("perdu")}
+			{document.write("<h1>Perdu !</h1>");
+			image3 = document.createElement("img");
+			image3.src = "images/triste.gif";
+			document.body.appendChild(image3);
+			}
 		break;
     case "ciseaux" :
             if (player == "pierre")  
-            {document.write("gagné")}
+            	{document.write("<h1>Gagné !</h1>");
+				image2 = document.createElement("img");
+				image2.src = "images/trophy.gif";
+				document.body.appendChild(image2);
+				}
             else
-            {document.write("perdu")}
+            	{document.write("<h1>Perdu !</h1>");
+				image3 = document.createElement("img");
+				image3.src = "images/triste.gif";
+				document.body.appendChild(image3);
+				}
             break;
 		}
 }
